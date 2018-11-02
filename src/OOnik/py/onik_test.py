952f5853c2740d1dba191fffed3f216633586c1b
EliseyP <shtangencirkul@gmail.com>
2018-11-02 21:37:16 +0300

    1.1.11
    Разделение на модули
    Исправления в regs

--- src/OOnik/py/onik_test.py
+++ src/OOnik/py/onik_test.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # _*_ coding: utf-8
+
 import sys
-from onik import get_string_converted
+import argparse
+sys.path.insert(0, './pythonpath')
+from onik_run import get_string_converted
+
+# def create_parser():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-t', '--titlo', choices=['on', 'off', 'open'], default='on')
+#
+#     return parser
+
+# FIXME: some errors in regs
+# застѣнкахъ претерпевшихъ
+# ѻ҆мрача́етъ
+# ѻ҆сквернѧетъ
+
 for line in sys.stdin:
-    sys.stdout.write(get_string_converted(line, titles_flag='on'))
+    # print("== ", line)
+    converted = get_string_converted(line, titles_flag='on') + '\n'
+    sys.stdout.write(converted)
